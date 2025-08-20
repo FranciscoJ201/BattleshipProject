a7916656c6340de71140860a1cdb9bb6a967c4f0
FranciscoJ201 <francisco.jimenez40@qmail.cuny.edu>
2025-08-19 21:14:55 -0400

    Small Updates

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 from ship import Ship
 
 class Board:
-    def __init__(self,size,ships):
+    def __init__(self,size):
         self.size = size
         self.ships = []
         self.shots = set()
@@ -12,14 +12,21 @@ class Board:
                 row.append(0)
             grid.append(row)
         self.grid = grid
-        
-
-
     def __str__(self):
         return '\n'.join(' '.join(map(str, row)) for row in self.grid)
-
+    def can_place(self,ship):
+        pass
+    def valid_move(self,move):
+        if move[0] >= 0 and move[0] <self.size and move[1] >= 0 and move[1] <self.size and (move not in self.shots) :
+            return True
+        else: 
+            return False
+        
+        
 
 tester = Board(10)
 tester.grid[0][1]=2
 tester.grid[0][0]=1
-print(tester)
+tester.shots.add((3,1))
+print(tester.valid_move((3,1)))     
+# print(tester)
--- ship.py
+++ ship.py
@@ -1,17 +1,31 @@
 class Ship:
-    def __init__(self,c1,c2):
-        self.c1 = c1
-        self.c2 = c2
+    def __init__(self, c1, c2):
+        x1, y1 = c1
+        x2, y2 = c2
         self.cells = []
-        if c1[0] == c2[0]:
-            for i in range(c2[1]+1):
-                self.cells.append((c1[0],c2[1]-i))
-        elif c1[1] == c2[1]:
-            for i in range(c2[0]+1):
-                self.cells.append((c1[0]+i,c2[1]))
-            
-newShip = Ship((0,4),(0,0))
-print(newShip.c1,newShip.c2)
-print(newShip.cells)
+        self.floating = True
+        if x1 == x2:  # vertical
+            y1, y2 = sorted((y1, y2))
+            self.c1, self.c2 = (x1, y1), (x2, y2)
+            for y in range(y1, y2 + 1):
+                self.cells.append((x1, y))
+        elif y1 == y2:  # horizontal
+            x1, x2 = sorted((x1, x2))
+            self.c1, self.c2 = (x1, y1), (x2, y2)
+            for x in range(x1, x2 + 1):
+                self.cells.append((x, y1))
+        else:
+            raise ValueError("Ships must be horizontal or vertical (same x or same y).")
+        
+    def __len__(self):
+        return len(self.cells)
+    def __str__(self):
+        return f"Ship (c1={self.c1}, c2={self.c2}, cells={self.cells})"
 
-#if c2 isnt greater it bugs out fix it
+
+# demo
+newShip = Ship((0,4), (0,0))
+# print(newShip.c1, newShip.c2)  # -> (0, 0) (0, 4)
+# print(newShip.cells)           # -> [(0,0),(0,1),(0,2),(0,3),(0,4)]
+# print(len(newShip))
+# print(newShip)
